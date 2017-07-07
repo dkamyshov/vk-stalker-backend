f4709b642edf959a9b5f00d30227b061c6a11933
Danil Kamyshov <danil.kamyshov@gmail.com>
2017-07-07 14:25:40 +0300

    refactored batch updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const admin_id = 21768456;
-
-const { vk_secret, jwt_secret, listen_address } = require("./secrets.js");
+const { vk_secret, listen_address } = require("./secrets.js");
 
 const VK = {
+    admin_id: 21768456,
     app_id: 6098516,
     secret_key: vk_secret,
     api_version: '5.56',
@@ -10,7 +9,6 @@ const VK = {
     redirect_uri: 'http://37.98.162.168:9000/verify',
     auth_uri: 'https://oauth.vk.com/authorize',
     access_token_uri: 'https://oauth.vk.com/access_token',
-
     friends_get_uri: 'https://api.vk.com/method/friends.get',
     users_get_uri: 'https://api.vk.com/method/users.get'
 };
@@ -27,6 +25,7 @@ const jprequest = require('./helpers/jprequest.js');
 
 const mongourl = "mongodb://localhost:27017/vkwatcher";
 
+const logger = require('./middleware/logger.js')(mongodb, mongourl);
 const rejectUnauthorized = require('./middleware/unauthorized.js');
 
 let app = express();
@@ -42,35 +41,41 @@ app.use(function(req, res, next) {
 
 app.post(
     '/api/stats.get',
+    logger,
     rejectUnauthorized,
     require('./handlers/api.stats.get.js')(mongodb, mongourl)
 );
 
 app.post(
     '/api/token.verify',
+    logger,
     require('./handlers/api.token.verify.js')()
 );
 
 app.post(
     '/api/dashboard.get',
+    logger,
     rejectUnauthorized,
     require('./handlers/api.dashboard.get.js')(mongodb, mongourl)
 );
 
 app.post(
     '/api/pause',
+    logger,
     rejectUnauthorized,
     require('./handlers/api.pause.js')(mongodb, mongourl)
 );
 
 app.post(
     '/api/user.get',
+    logger,
     rejectUnauthorized,
     require('./handlers/api.user.get.js')(mongodb, mongourl)
 );
 
 app.post(
     '/api/token.create',
+    logger,
     require('./handlers/api.token.create.js')(mongodb, mongourl, VK)
 );
 
@@ -100,126 +105,75 @@ const updateBalances = balances => {
             })
         );
     }).then(results => {
-        mdb.collection('journal').insert({
-            time: Date.now(),
-            payload: `Balances updated.`
-        });
         mdb.close();
     }).catch(e => {
-        mdb.collection('journal').insert({
-            time: Date.now(),
-            payload: `Failed to update balances: ${e.message}`
-        });
         mdb.close();
     });
 }
 
 setInterval(function() {
-    let mdb, subtractBalance = Object.create(null);
+    let _db, subtractBalance = Object.create(null);
 
-    mongodb.connect(mongourl).then(db => {
-        return (mdb = db).collection('settings').aggregate([
-            { $lookup: {
-                from: 'users',
-                localField: 'id',
-                foreignField: 'owner',
-                as: 'users'
-            } }, 
-            { $project: { _id: 0, id: 1, pause: 1, balance: 1, users: 1 } }
-        ]).toArray();
-    }).then(list => {
-        let uids = Object.create(null);
+    const $query = [
+        { $match: { $and: [ {pause: false }, { balance: { $gt: 0 } } ] } }
+    ];
 
-        list.map(user => {
-            subtractBalance[user.id] = 0;
+    mongodb.connect(mongourl)
+    .then(db => (_db = db).collection('settings').aggregate($query).toArray())
+    .then(list => {
+        let uids = Object.create(null), batches = [];
 
-            if(!user.pause && user.balance >= user.users.length) {
-                user.users.map(user2 => {
-                    uids[user2.id] = uids[user2.id] ? [...uids[user2.id], user.id] : [user.id];
-                })
-            }
+        list.map((user, i) => {
+            const bucket = Math.floor(i/250);
+
+            batches[bucket] = Object.assign(batches[bucket] || {}, { [user._id]: user.owners });
         });
 
-        const uidsArray = Object.keys(uids).map(k => k);
+        return Promise.all(
+            batches.map((batch, i) => {
+                return new Promise(function(resolve, reject) {
+                    setTimeout(function() {
+                        jprequest(buildURI(
+                            VK.users_get_uri,
+                            { user_ids: Object.keys(batch).join(','),
+                              fields: 'online',
+                              v: VK.api_version }
+                        ))
+                        .then(response => {
+                            const now = Date.now();
 
-        let batches = [];
+                            _db.collection('records').insertMany(
+                                response.response.map(user => {
+                                    batch[user.id].map(owner => {
+                                        subtractBalance[owner] += 1;
+                                    })
 
-        for(let i = 0; i < uidsArray.length/250; ++i) {
-            batches.push(uidsArray.slice(i*250, i*250+250));
-        }
-
-        mdb.collection('journal').insert({
-            time: Date.now(),
-            payload: `${list.length} accounts, ${uidsArray.length} users, ${batches.length} batches.`
-        });
-
-        let batchesGoing = batches.length;
-
-        for(let i = 0; i < batches.length; ++i) {
-            (function(batch, index) {
-                setTimeout(function() {
-                    jprequest(buildURI(
-                        VK.users_get_uri,
-                        {
-                            user_ids: batch.join(','),
-                            fields: 'online',
-                            v: VK.api_version
-                        }
-                    )).then(response => {
-                        const now = Date.now();
-                        mdb.collection('records').insertMany(
-                            response.response.map(user => {
-                                const owners = uids[user.id];
-                                owners.map(owner => {
-                                    subtractBalance[owner] += 1;
-                                });
-
-                                return {
-                                    id: user.id,
-                                    t: now,
-                                    s: user.online_mobile ? 2 : user.online
-                                };
+                                    return {
+                                        id: user.id,
+                                        t: now,
+                                        s: user.online_mobile ? 2 : user.online
+                                    };
+                                })
+                            )
+                            .then(() => {
+                                resolve();
                             })
-                        ).then(result => {
-                            if(--batchesGoing == 0) {
-                                mdb.collection('journal').insert({
-                                    time: Date.now(),
-                                    payload: `Done all batches.`
-                                });
-                                updateBalances(subtractBalance);
-                                mdb.close();
-                            }
-                        }).catch(e => {
-                            mdb.collection('journal').insert({
-                                time: Date.now(),
-                                payload: `Batch error: ${e.message}.`
-                            });
-                            if(--batchesGoing == 0) {
-                                updateBalances(subtractBalance);
-                                mdb.close();
-                            }
-                        });
-                    }).catch(e => {
-                        mdb.collection('journal').insert({
-                            time: Date.now(),
-                            payload: `Batch error: ${e.message}.`
-                        });
-                        if(--batchesGoing == 0) {
-                            updateBalances(subtractBalance);
-                            mdb.close();
-                        }
-                    });
-                }, 500*index);
-            })(batches[i], i);
-        }
+                            .catch(() => {
+                                reject();
+                            })
+                        })
+                    }, i*500);
+                });
+            })
+        );
+    })
+    .then(results => {
+        console.log("Done all batches!");
+        _db.close();
     })
     .catch(e => {
         console.error(e);
-        mdb.collection('journal').insert({
-            time: Date.now(),
-            payload: `DB connection error: ${e.message}.`
-        });
-        mdb.close();
+        _db.close();
     });
 }, 60000);
 
